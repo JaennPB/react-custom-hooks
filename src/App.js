697304e21cf527eaa543db92edd71bc1a0829393
jaennpb <jaenn.bucio@gmail.com>
2021-04-26 19:38:19 -0500

    added custom hook

--- src/App.js
+++ src/App.js
@@ -1,39 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from "react";
+import useHttp from "./hooks/useHttp";
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
-      );
+  const transformData = (dataObject) => {
+    const loadedTasks = [];
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+    for (const taskKey in dataObject) {
+      loadedTasks.push({ id: taskKey, text: dataObject[taskKey].text });
     }
-    setIsLoading(false);
+
+    setTasks(loadedTasks);
   };
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp(
+    { url: "https://task-d04c9-default-rtdb.firebaseio.com/tasks.json" },
+    transformData
+  );
 
   useEffect(() => {
     fetchTasks();
